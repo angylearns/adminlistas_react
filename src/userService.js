acf0b62f3356cab9d4ca52153eafafb897ba832f
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-02-02 08:58:48 +0100

    Clean empty spaces

--- src/userService.js
+++ src/userService.js
@@ -5,8 +5,8 @@ const apiClient = axios.create({
     baseURL: 'http://localhost:3000/',
     withCredentials: false,
     headers: {
-      "Accept": "application/json",
-     "Content-Type": "application/json"
+        "Accept": "application/json",
+        "Content-Type": "application/json"
     }
 })
 
